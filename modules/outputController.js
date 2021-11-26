1014c1db419568993b2998114829f904ef1ae973
Kirschn <mala.wolff00@gmail.com>
2021-11-26 18:55:40 +0100

    raspi output

--- modules/outputController.js
+++ modules/outputController.js
@@ -1,9 +1,30 @@
+if (global.stateMachineConfig.raspberryPi.installed) {
+    import pigpio from 'pigpio';
+}
+
 export default class covcheckOutputModule {
     constructor() {
+        if (global.stateMachineConfig.raspberryPi.installed) {
+            this.Gpio = pigpio.Gpio;
+            this.doorPin = new this.Gpio(global.stateMachineConfig.raspberryPi.doorRelaisGPIO, {
+                "mode": this.Gpio.OUTPUT
+            });
+        }
 
     }
     allowEntrance(stateMachine) {
         console.log("Allowing Entrance")
         stateMachine.hold = true;
+        this.piWriteDoor(true);
+        setTimeout(() => {
+            this.piWriteDoor(false);
+            stateMachine.reset();
+            stateMachine.holf = false;
+        }, global.config.stateMachineConfig.raspberryPi.doorOpenTime)
+    }
+    piWriteDoor(val) {
+        if (!global.stateMachineConfig.raspberryPi.installed)
+            return;
+        this.doorPin.digitalWrite(val)
     }
 }
