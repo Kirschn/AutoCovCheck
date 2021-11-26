2b02e0777accc31c04b66e1df2ce83b78da4437f
Kirschn <mala.wolff00@gmail.com>
2021-11-26 19:33:11 +0100

    remove package lock

--- install.sh
+++ install.sh
@@ -2,13 +2,14 @@
 
 curl -fsSL https://deb.nodesource.com/setup_17.x | bash -
 
-apt install -y git openssl nodejs npm
+apt install -y git openssl nodejs
 
 git clone --recurse-submodules https://github.com/Kirschn/AutoCovCheck
 
 cd AutoCovCheck
 
 host="$(hostname)"
+mkdir sslcert
 openssl req -x509 -out sslcert/server.crt -keyout sslcert/server.key \
   -newkey rsa:2048 -nodes -sha256 \
   -subj "/CN=$host" -extensions EXT -config <( \
