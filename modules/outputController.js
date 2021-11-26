951bb0a1c6b67030e9bb459593b6865b052864f8
Kirschn <mala.wolff00@gmail.com>
2021-11-26 21:09:49 +0100

    fix pigpio module import

--- modules/outputController.js
+++ modules/outputController.js
@@ -1,12 +1,15 @@
-if (global.stateMachineConfig.raspberryPi.installed) {
-    import pigpio from 'pigpio';
+var pigpio = null;
+if (global.config.stateMachineConfig.raspberryPi.installed) {
+    import("pigpio").then(module => {
+        pigpio = module
+    })
 }
 
 export default class covcheckOutputModule {
     constructor() {
-        if (global.stateMachineConfig.raspberryPi.installed) {
+        if (global.config.stateMachineConfig.raspberryPi.installed) {
             this.Gpio = pigpio.Gpio;
-            this.doorPin = new this.Gpio(global.stateMachineConfig.raspberryPi.doorRelaisGPIO, {
+            this.doorPin = new this.Gpio(global.config.stateMachineConfig.raspberryPi.doorRelaisGPIO, {
                 "mode": this.Gpio.OUTPUT
             });
         }
@@ -23,7 +26,7 @@ export default class covcheckOutputModule {
         }, global.config.stateMachineConfig.raspberryPi.doorOpenTime)
     }
     piWriteDoor(val) {
-        if (!global.stateMachineConfig.raspberryPi.installed)
+        if (!global.config.stateMachineConfig.raspberryPi.installed)
             return;
         this.doorPin.digitalWrite(val)
     }
