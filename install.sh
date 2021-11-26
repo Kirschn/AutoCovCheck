b5d269eae01e76c094d591eab168aedae3dcd873
Kirschn <mala.wolff00@gmail.com>
2021-11-26 19:11:44 +0100

    install script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+curl -fsSL https://deb.nodesource.com/setup_17.x | bash -
+
+apt install -y git openssl nodejs npm
+
+git clone --recurse-submodules https://github.com/Kirschn/AutoCovCheck
+
+cd AutoCovCheck
+
+host="$(hostname)"
+openssl req -x509 -out sslcert/server.crt -keyout sslcert/server.key \
+  -newkey rsa:2048 -nodes -sha256 \
+  -subj "/CN=$host" -extensions EXT -config <( \
+   printf "[dn]\nCN=$host\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:$host\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
+
+npm install
